3365268172c024158163187f00d535852a931b11
Ben Collins (Novatech) <BenC@ntetogo.com>
2018-02-13 11:22:25 -0600

    i like turtles

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -12,6 +12,7 @@ class SearchBar extends Component {
 
         this.search = this.search.bind(this);
         this.handleTermChange = this.handleTermChange.bind(this);
+        this.handleKeyPress = this.handleKeyPress.bind(this);
     }
 
     search() {
@@ -23,10 +24,16 @@ class SearchBar extends Component {
         this.setState({term});
     }
 
+    handleKeyPress(e) {
+        if (e.key === "Enter") {
+            this.props.onSearch(this.state.term);
+        }
+    }
+
     render() {
         return (
             <div className="SearchBar">
-                <input placeholder="Enter A Song, Album, or Artist" onChange={this.handleTermChange} />
+                <input placeholder="Enter A Song, Album, or Artist" onChange={this.handleTermChange} onKeyPress={this.handleKeyPress} />
                 <a onClick={this.search}>SEARCH</a>
             </div>
         );
